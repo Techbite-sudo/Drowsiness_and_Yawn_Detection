2fa7fceb5888831efbf2cf67febb6abbcf04bd3c
Techbite <bonfacemwema7@gmail.com>
2024-02-29 13:37:02 +0300

    custom user creation

--- drowsiness_app/models.py
+++ drowsiness_app/models.py
@@ -1,6 +1,20 @@
-import uuid
+# drowsiness_detection/models.py
+
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# create your models here
 
 
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ("admin", "Admin"),
+        ("driver", "Driver"),
+        ("car_owner", "Car Owner"),
+    ]
+
+    user_role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default="driver",
+    )
