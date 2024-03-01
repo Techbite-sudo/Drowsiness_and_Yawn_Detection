e82ddd49ef3a123e11bd9bff97ceb72b6ee2cfe2
Techbite <bonfacemwema7@gmail.com>
2024-03-01 11:56:30 +0300

    registration: fix

--- drowsiness_project/settings.py
+++ drowsiness_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'drowsiness_app',
 ]
 
@@ -130,6 +131,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 LOGIN_URL = 'login'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
